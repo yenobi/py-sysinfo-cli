92670a873ebcdac7a8f0e695390ca246d3f15538
yenobi <yenobi@pm.me>
2026-09-21 23:12:08 +0300

    add draft of the steps how the cli will work

--- src/sysinfo/cli.py
+++ src/sysinfo/cli.py
@@ -1,2 +1,9 @@
 def main() -> None:
+    # args = parse_args()
+
+    # data = collect_data()
+    # formatted = format_data(data)
+    # output = render(formatted)
+
+    # print(output)
     print("sysinfo")
